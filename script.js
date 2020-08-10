29da891aea7f035b24704a39a04c062df993ba25
Luis Antonio de Souza <dev.luis2003@gmail.com>
2020-08-10 15:00:24 -0300

    definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -22,7 +22,22 @@ function criarCobrinha() {
   }
 }
 
+document.addEventListener('keydown', update)
+
+function update(e) {
+  if(e.keyCode == 37 && direction != 'right') direction = 'left'
+  if(e.keyCode == 38 && direction != 'down') direction = 'up'
+  if(e.keyCode == 39 && direction != 'left') direction = 'right'
+  if(e.keyCode == 40 && direction != 'up') direction = 'down'
+}
+
 function iniciarJogo() {
+  if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
+  if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
+  if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
+  if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
+
+
   criarBG()
   criarCobrinha()
 
