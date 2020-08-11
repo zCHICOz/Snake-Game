65213c762f0bb0d07ce162a144fc761ba5c757f8
Luis Antonio de Souza <dev.luis2003@gmail.com>
2020-08-11 14:00:54 -0300

    adicionar estilização para a pagina

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ function iniciarJogo() {
   if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
   if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
 
+  for(i = 1; i < snake.length; i++) {
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(jogo)
+      alert('Game over :(')
+    }
+  }
+
 
   criarBG()
   criarCobrinha()
@@ -65,7 +72,6 @@ function iniciarJogo() {
     food.x = Math.floor(Math.random() * 15 + 1) * box;
     food.y = Math.floor(Math.random() * 15 + 1) * box
   }
-
 
   let newHead = {
     x: snakeX,
